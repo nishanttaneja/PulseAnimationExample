8f2157d8224bc0261951955bd186743864d545c4
Nishant Taneja <nishant.taneja2701@gmail.com>
2022-01-07 23:54:48 +0530

    create package

---
+++ PulseAnimation/README.md
@@ -0,0 +1,3 @@
+# PulseAnimation
+
+A description of this package.
---
+++ PulseAnimation/Sources/PulseAnimation/PulseAnimation.swift
@@ -0,0 +1,6 @@
+public struct PulseAnimation {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
